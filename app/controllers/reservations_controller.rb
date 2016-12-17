e296c079bd4b60c103771e83c7eefd5a12592b82
James Novakowski <james.novakowski@gmail.com>
2016-12-17 14:31:13 -0500

    Added create method to reservations. Added guests to reservations.  Some styling changes to restaurants/index

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,14 +1,25 @@
 class ReservationsController < ApplicationController
-	#before_action :set_restaurant, only: [:new]
+	before_action :set_restaurant, only: [:new, :create]
 
   def new
   	@reservation = Reservation.new
-  	@restaurant = Restaurant.find(params[:restaurant_id])
-  	
+  	@restaurant
   end
 
   def create
-    puts params
+    validated_params = reservation_params
+    @reservation = Reservation.new(validated_params)
+    @reservation[:user_id] = current_user.id
+    @reservation[:restaurant_id] = params[:restaurant_id]
+    respond_to do |format|
+      if @reservation.save
+        format.html { redirect_to @restaurant, notice: 'Reservation was successfully created.' }
+        format.json { render :show, status: :created, location: @reservation }
+      else
+        format.html { render :new }
+        format.json { render json: @reservation.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
@@ -22,6 +33,13 @@ class ReservationsController < ApplicationController
 
   private
 
-  
+  def reservation_params
+    params.require(:reservation).permit(:num_guests, :comment, :res_date, :res_time, :restaurant_id)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
 
 end
